2341327bbb469871a3ed783f583a0ae9ea3f6203
rfrontz <ryanfrontz@hotmail.com>
2020-01-24 14:51:55 -0500

    Hello Git program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Ryan Frontz
+ *        Date: 1/24/2020
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout<< "Hello Git!!" << endl;
+    return 0;
+}// main
